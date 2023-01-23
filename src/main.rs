4778914967723aae23f82acf29d797626382be1b
wired-maya <dhodul@gmail.com>
2023-01-23 02:12:47 -0800

    sync with engine

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,11 @@ extern crate cinema_skylight_engine;
 use self::glfw::{Context, Key, Action};
 use std::{sync::mpsc::Receiver, ffi::{c_void, CString}, slice, error::Error, rc::Rc};
 use silver_gl::*;
-use cgmath::{vec3, Point3, Matrix4, SquareMatrix, Quaternion, Euler, Deg, Rad};
+use cgmath::{vec3, Quaternion, Euler, Deg};
 use cinema_skylight_engine::*;
 
-const WIDTH: i32 = 800;
-const HEIGHT: i32 = 600;
+const WIDTH: i32 = 1280;
+const HEIGHT: i32 = 720;
 const FOV: f32 = 45.0;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -58,13 +58,6 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut resource_manager = ResourceManager::default();
 
-    let model_shader_program = resource_manager.load_shader_program(
-        ShaderPathBundle {
-            vertex: Some("assets/shaders/shader.vert".to_string()),
-            geometry: None,
-            fragment: Some("assets/shaders/deffered_shader.frag".to_string())
-        }
-    )?;
     let framebuffer_shader_program = resource_manager.load_shader_program(
         ShaderPathBundle {
             vertex: Some("assets/shaders/framebuffer.vert".to_string()),
@@ -72,33 +65,39 @@ fn main() -> Result<(), Box<dyn Error>> {
             fragment: Some("assets/shaders/framebuffer.frag".to_string())
         }
     )?;
-    let skybox_shader_program = resource_manager.load_shader_program(
+
+    let render_pipeline = View3DRenderPipeline::new(
+        &mut resource_manager,
+        WIDTH,
+        HEIGHT,
         ShaderPathBundle {
-            vertex: Some("assets/shaders/skybox.vert".to_string()),
+            vertex: Some("assets/shaders/framebuffer.vert".to_string()),
             geometry: None,
-            fragment: Some("assets/shaders/skybox.frag".to_string())
-        }
-    )?;
-    let blur_shader_program = resource_manager.load_shader_program(
+            fragment: Some("assets/shaders/lighting_pass_shader.frag".to_string())
+        },
         ShaderPathBundle {
             vertex: Some("assets/shaders/framebuffer.vert".to_string()),
             geometry: None,
             fragment: Some("assets/shaders/gaussian_blur.frag".to_string())
         }
     )?;
-    let lighting_pass_shader_program = resource_manager.load_shader_program(
+
+    let mut scene = View3DScene::new(
+        &mut resource_manager,
         ShaderPathBundle {
-            vertex: Some("assets/shaders/framebuffer.vert".to_string()),
+            vertex: Some("assets/shaders/shader.vert".to_string()),
             geometry: None,
-            fragment: Some("assets/shaders/lighting_pass_shader.frag".to_string())
-        }
+            fragment: Some("assets/shaders/deffered_shader.frag".to_string())
+        },
+        "assets/textures/skybox/full.jpg",
+        ShaderPathBundle {
+            vertex: Some("assets/shaders/skybox.vert".to_string()),
+            geometry: None,
+            fragment: Some("assets/shaders/skybox.frag".to_string())
+        },
+        CameraSize { width: WIDTH, height: HEIGHT, fov: FOV },
+        Box::new(render_pipeline)
     )?;
-
-    let mut world_obj = GameObject::new();
-
-    let skybox = resource_manager.load_skybox("assets/textures/skybox/full.jpg")?;
-    // let mut skybox_obj = GameObject::from_model(skybox);
-    // world_obj.children.push(skybox_obj);
 
     let distance_scale = 2.0;
     let amount = 4;
@@ -112,32 +111,15 @@ fn main() -> Result<(), Box<dyn Error>> {
             planet_obj.position = vec3(x as f32, 0.0, z as f32) * distance_scale;
             planet_obj.scale = distance_scale / 10.0;
 
-            world_obj.children.push(planet_obj);
+            scene.world_obj.children.push(planet_obj);
         }
     }
 
-    let render_pipeline = View3DRenderPipeline::new(
-        WIDTH,
-        HEIGHT,
-        lighting_pass_shader_program,
-        blur_shader_program
-    )?;
+    scene.models.push(planet_model);
 
     let mut default_framebuffer = Framebuffer::new_default(WIDTH, HEIGHT);
 
-    default_framebuffer.link_to(render_pipeline.get_link().unwrap());
-
-    let mut camera = Camera::new(WIDTH, HEIGHT, FOV, vec![&model_shader_program, &skybox_shader_program]).unwrap();
-    camera.position = Point3 { x: 0.0, y: 0.0, z: 1.0 };
-
-    let mut scene = View3DScene {
-        models: vec![planet_model],
-        model_shader_program,
-        skybox,
-        skybox_shader_program,
-        camera,
-        render_pipeline: Box::new(render_pipeline),
-    };
+    default_framebuffer.link_to(scene.render_pipeline.get_link().unwrap());
 
     // Set exposure
     framebuffer_shader_program.use_program();
@@ -160,11 +142,11 @@ fn main() -> Result<(), Box<dyn Error>> {
             &mut default_framebuffer
         )?;
 
-        // let pos = vec3(0.0, current_frame.sin(), 0.0);
-        // world_obj.children[0].position = pos; // First planet
-        world_obj.children[0].rotation = Quaternion::from(Euler::new(Deg(0.0), Deg(current_frame * 25.0), Deg(0.0)));
+        // First planet
+        scene.world_obj.children[0].position = vec3(0.0, current_frame.sin(), 0.0);
+        scene.world_obj.children[0].scale = (current_frame * 3.0).cos() * 0.3;
+        scene.world_obj.children[0].rotation = Quaternion::from(Euler::new(Deg(0.0), Deg(current_frame * 100.0), Deg(0.0)));
 
-        world_obj.set_transform_to_drawable(Matrix4::identity());
         scene.draw()?;
         default_framebuffer.draw(&framebuffer_shader_program)?;
 
